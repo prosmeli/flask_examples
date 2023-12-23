5d43d710af52c869dcd5c6de42ef862f6954ef6f
prosmeli <panagmosx@hotmail.com>
2023-12-23 10:52:45 +0200

    commit 5

--- src/app2.py
+++ src/app2.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
@@ -7,6 +8,14 @@ app = Flask(__name__)
 @app.route('/<name>')
 def root(name = None):
     return render_template("index.html", my_name=name)
+
+@app.route('/books/')
+def books():
+    with open("my_books.json") as json_file:
+        my_data = json.load(json_file)
+        print(my_data)
+
+    return render_template("books.html", my_data = my_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
