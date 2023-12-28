df356422211c81e0784d750890331ce038268732
prosmeli <panagmosx@hotmail.com>
2023-12-28 07:45:03 +0200

    commit 12

--- src/app.py
+++ src/app.py
@@ -1,45 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/index/')
 @app.route('/')
 def root():
-    return """
-    <html>
-
-    <head>
-        <title>This is my first web page</title>
-    </head>
-
-    <body>
-        <h1>Hello World!</h1>
-        <p>This is a simple paragraph.</p>
-        <p>This is a second paragraph.</p>
-    </body>
-
-    </html>
-    """
-
-@app.route('/hello/')
-def hello():
-    return "Hello"
-
-@app.route('/hello2/<name>')
-def hello2(name):
-    return f"Hello {name}!"
-
-@app.route('/hello3/<int:id>')
-def hello3(id):
-    return f"Your id is: {id}!"
-
-@app.route('/hello4')
-@app.route('/hello4/<int:id>')
-def hello4(id = None):
-    if id == None:
-        return f"Id doesn't exist!"
-    else:
-        return f"Your id is: {id}!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True) # False when our app has been uploaded to production server.
+    app.run(debug=True)
