3248f5da38fc581fca86ac7aafa99edd29a5663b
prosmeli <panagmosx@hotmail.com>
2023-12-28 08:14:18 +0200

    commit 13

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -7,6 +7,22 @@ app = Flask(__name__)
 @app.route('/')
 def root():
     return render_template("index.html")
+
+@app.route('/signup/')
+def signup():
+    return render_template("signup.html")
+
+@app.route('/login/')
+def login():
+    return render_template("login.html")
+
+@app.route('/logout/')
+def logout():
+    return redirect(url_for('root')) # Δίνω το όνομα της σελίδα όπως είναι το όνομα της μεθόδου, δηλαδή root και όχι index ή κάτι άλλο.
+
+@app.route('/new_article/')
+def new_article():
+    return render_template("new_article.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
